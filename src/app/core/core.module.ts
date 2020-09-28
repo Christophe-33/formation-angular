c5c0969400ae314aade97f8fff623e057c3cc714
christophe-33 <chris__33@live.fr>
2020-09-28 16:16:45 +0200

    Création du routeur et de la route login

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [NavComponent, HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavComponent, HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
